6ebc0a5316c3bd108b61871cf36211cbc67fc8ac
matdelp <mathilde.delpeuch@gmail.com>
2025-10-14 09:29:29 +0200

    feat: add tests on quality / sell In increase / decrease for mentionned items

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,24 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+//Item("name", sellIn, quality)
+
+describe("Gilded Rose", () => {
+  it("should foo", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe("foo");
+  });
+
+  it("should decrease quality and sellIn by 1 for a common item", () => {
+    const gildedRose = new GildedRose([new Item("item", 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(19);
+  });
+  it("should increase quality and decrease sellIn by 1 for a Aged Brie", () => {
+    const gildedRose = new GildedRose([new Item("Aged Brie", 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(21);
   });
 });
